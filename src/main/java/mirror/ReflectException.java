4b86af5e3c244aa49a8f9dbd12163d2d620ab4c4
____lamer <157688302@qq.com>
2019-03-11 15:42:09 +0800

    update Reflect from VirtualApp

---
+++ src/main/java/mirror/ReflectException.java
@@ -0,0 +1,15 @@
+package mirror;
+
+/**
+ * @author Lody
+ */
+public class ReflectException extends RuntimeException {
+
+	public ReflectException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ReflectException(Throwable cause) {
+		super(cause);
+	}
+}
